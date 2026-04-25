1d743d8f3ed500b5dec50e0fc0bb2df3273d8438
Frank Wei <fwei1729@gmail.com>
2026-04-25 16:51:40 -0400

    Moved download to tmp/

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 set -e
 
 QUARTO_VERSION=1.9.37
+QUARTO_DIR=/tmp/quarto-${QUARTO_VERSION}
 
 # Install quarto
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
-tar -xzf quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
+wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz -O /tmp/quarto.tar.gz
+tar -xzf /tmp/quarto.tar.gz -C /tmp
 
 # Run render command in Quarto folder
-export PATH=$PWD/quarto-${QUARTO_VERSION}/bin:$PATH
+export PATH=${QUARTO_DIR}/bin:$PATH
 quarto render .
