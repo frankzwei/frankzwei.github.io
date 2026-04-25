1bf0b6d5de01a0485e183ee9121978761e10fae1
Frank Wei <fwei1729@gmail.com>
2026-04-25 16:40:12 -0400

    Added missing $

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 QUARTO_VERSION=1.9.37
 
 # Install quarto
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-{QUARTO_VERSION}-linux-amd64.tar.gz
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
 tar -xzf quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
 
 # Run render command in Quarto folder
